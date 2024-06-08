a2adfc4bd51240b3f7629189394cca37dbeb83a8
Ronald Huereca <ronald@mediaron.com>
2024-06-07 20:20:59 -0500

    Adding plugins to Ajaxify comments.

--- dist/wpac-admin-integrations-js.asset.php
+++ dist/wpac-admin-integrations-js.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-components', 'wp-i18n'), 'version' => '82619ae89cea9bb6f3f4');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-components', 'wp-i18n'), 'version' => 'ef2fdff03258ff618b2a');
--- src/js/react/views/Integrations/integrations.js
+++ src/js/react/views/Integrations/integrations.js
@@ -18,6 +18,9 @@ import SendCommand from '../../utils/SendCommand';
 import Notice from '../../components/Notice';
 import ConfettiImg from './img/confetti-256x256.png';
 import CommentEditCoreImg from './img/cec-256x256.png';
+import QDLXImg from './img/qdlx-256x256.png';
+import HasImg from './img/has-256x256.png';
+import AlertsImg from './img/alerts-256x256.png';
 import Plugin from './Plugin';
 
 const integratedPlugins = [
@@ -45,12 +48,37 @@ const integratedPlugins = [
 	},
 ];
 
+const dlxPlugins = [
+	{
+		icon: HasImg,
+		pluginName: 'Highlight and Share',
+		path: 'highlight-and-share/highlight-and-share.php',
+		installed: false,
+		activated: false,
+		orgUrl: 'https://wordpress.org/plugins/highlight-and-share/',
+		description: 'Allow users to highlight text and see social media sharing options. It also supports inline highlighting and has a Click to Tweet block.',
+		installNonce: wpacAdminIntegrations.hasInstallNonce,
+		activateNonce: wpacAdminIntegrations.hasActivateNonce,
+	},
+	{
+		icon: AlertsImg,
+		pluginName: 'AlertsDLX',
+		path: 'alerts-dlx/alerts-dlx.php',
+		installed: false,
+		activated: false,
+		orgUrl: 'https://wordpress.org/plugins/alerts-dlx/',
+		description: 'Create impactful alerts on your site with four different alert types and endless variations.',
+		installNonce: wpacAdminIntegrations.alertsInstallNonce,
+		activateNonce: wpacAdminIntegrations.alertsActivateNonce,
+	},
+];
+
 const IntegrationsScreen = ( props ) => {
 	const getCommentEditingHeader = () => {
 		return (
 			<>
 				<h2>
-					{ __( 'Plugin Integrations and Compatibility', 'wp-ajaxify-comments' ) }
+					{ __( 'Available Plugin Integrations', 'wp-ajaxify-comments' ) }
 				</h2>
 				<p className="description">
 					{ __(
@@ -77,12 +105,23 @@ const IntegrationsScreen = ( props ) => {
 		);
 	};
 
+	const getOtherPlugins = () => {
+		return (
+			<div className="ajaxify-plugin-integrations-wrapper">
+				{ dlxPlugins.map( ( plugin ) => {
+					return (
+						<Plugin
+							key={ plugin.path }
+							{ ...plugin }
+						/>
+					);
+				} ) }
+			</div>
+		);
+	};
+
 	return (
 		<>
-			<div className="ajaxify-admin-panel-area">
-				{ getCommentEditingHeader() }
-				{ getPlugins() }
-			</div>
 			<div className="ajaxify-admin-panel-area">
 				<>
 					<h2>
@@ -90,25 +129,60 @@ const IntegrationsScreen = ( props ) => {
 					</h2>
 					<p className="description">
 						{ __(
-							'These pro/premium plugins are designed to work with Ajaxify Comments.',
+							'Please check out these pro offerings from the makers of this plugin.',
 							'wp-ajaxify-comments',
 						) }
 					</p>
 				</>
-				<div className="ajaxify-plugin-integration">
-					<div className="ajaxify-plugin-integration-info">
-						<div className="ajaxify-plugin-integration-icon">
-							<img src={ CommentEditCoreImg } alt={ __( 'Comment Edit Pro', 'wp-ajaxify-comments' ) } />
+				<div className="ajaxify-plugin-integrations-wrapper">
+					<div className="ajaxify-plugin-integration">
+						<div className="ajaxify-plugin-integration-info">
+							<div className="ajaxify-plugin-integration-icon">
+								<img src={ CommentEditCoreImg } alt={ __( 'Comment Edit Pro', 'wp-ajaxify-comments' ) } />
+							</div>
+							<div className="ajaxify-plugin-integration-meta">
+								<h3>
+									<a href="https://dlxplugins.com/plugins/comment-edit-pro">{ __( 'Comment Edit Pro', 'wp-ajaxify-comments' ) }</a></h3>
+								<p className="description">
+									{ __( 'Comment Edit Pro extends basic comment editing with frontend moderation, webhooks, Slack integration, comment shortcuts, and much, much more.', 'wp-ajaxify-comments' ) }
+								</p>
+							</div>
 						</div>
-						<div className="ajaxify-plugin-integration-meta">
-							<h3>
-								<a href="https://dlxplugins.com/plugins/comment-edit-pro">{ __( 'Comment Edit Pro', 'wp-ajaxify-comments' ) }</a></h3>
-							<p className="description">
-								{ __( 'Comment Edit Pro extends basic comment editing with frontend moderation, webhooks, Slack integration, comment shortcuts, and much, much more.', 'wp-ajaxify-comments' ) }
-							</p>
+					</div>
+					<div className="ajaxify-plugin-integration">
+						<div className="ajaxify-plugin-integration-info">
+							<div className="ajaxify-plugin-integration-icon">
+								<img src={ QDLXImg } alt={ __( 'QuotesDLX', 'wp-ajaxify-comments' ) } />
+							</div>
+							<div className="ajaxify-plugin-integration-meta">
+								<h3>
+									<a href="https://dlxplugins.com/plugins/quotesdlx">{ __( 'QuotesDLX', 'wp-ajaxify-comments' ) }</a></h3>
+								<p className="description">
+									{ __( 'QuotesDLX provides several beautiful themes you can use to showcase quotes on your site. It works with the block editor and via shortcode.', 'wp-ajaxify-comments' ) }
+								</p>
+							</div>
 						</div>
 					</div>
 				</div>
+
+			</div>
+			<div className="ajaxify-admin-panel-area">
+				{ getCommentEditingHeader() }
+				{ getPlugins() }
+			</div>
+			<div className="ajaxify-admin-panel-area">
+				<>
+					<h2>
+						{ __( 'You might also like…', 'wp-ajaxify-comments' ) }
+					</h2>
+					<p className="description">
+						{ __(
+							'These plugins can help you with social sharing and creating impactful alerts.',
+							'wp-ajaxify-comments',
+						) }
+					</p>
+				</>
+				{ getOtherPlugins() }
 			</div>
 		</>
 	);
