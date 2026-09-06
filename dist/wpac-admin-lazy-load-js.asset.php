0c41cf43f06516a848c2d1565bdfb3fdd8563b92
Ronald Huereca <ronald@mediaron.com>
2026-09-05 21:12:27 -0500

    Adding lazy load options and update scripts.

--- dist/wpac-admin-lazy-load-js.asset.php
+++ dist/wpac-admin-lazy-load-js.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-components', 'wp-i18n'), 'version' => 'd3ed668d8ee8bfa5aa1b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-components', 'wp-i18n'), 'version' => 'd1e235ef6bdabdc16b2d');
